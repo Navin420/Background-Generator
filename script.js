a4955ef6c2425fdc4dc516d0d1a01b295410acab
Navin420 <bhattarnaveen01@gmail.com>
2020-06-14 04:15:15 +0530

    changes to fix h3 text and body background color bug during intial loading time

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// import { without } from 'lodash';
+// console.log(without);
+
+var _ = require('lodash');
+console.log(_);
+var array = [1,2,3,4,3,5,3,2,5];
+console.log('answer:', _.without(array, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
@@ -13,6 +21,10 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+
+//for setting value of linear gradient info as text in h3
+// and also to set values of linear-gradient in body.style.background
+setGradient();
 
 color1.addEventListener("input", setGradient);
 
